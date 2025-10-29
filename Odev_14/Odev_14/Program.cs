a2b12603533f1123692db8050408d6dd55d8ba25
Barbaros Emre Alagöz <bealagoz@aof.anadolu.edu.tr>
2025-10-29 14:56:59 +0300

    Add files via upload
    Learning C#

---
+++ Odev_14/Odev_14/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Odev_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dortislem drt = new dortislem();
+            drt.topla(20, 10);
+            drt.cikar(20, 10);
+            drt.carp(20, 10);
+            drt.bol(20, 10);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Odev_14/Odev_14/dortislem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_14
+{
+    internal class dortislem
+    {
+        public int topla(int s1, int s2)
+        {
+            int s3 = s1 + s2;
+            Console.WriteLine("Sonuç: " + s3);
+            return s3;
+        }
+
+        public int cikar(int s1, int s2)
+        {
+            int s4 = s1 - s2;
+            Console.WriteLine("Sonuç: " + s4);
+            return s4;
+        }
+
+        public int carp(int s1, int s2)
+        {
+            int s5 = s1 * s2;
+            Console.WriteLine("Sonuç: " + s5);
+            return s5;
+        }
+
+        public int bol(int s1, int s2)
+        {
+            int s6 = s1 / s2;
+            Console.WriteLine("Sonuç: " + s6);
+            return s6;
+        }
+
+
+    }
+}
