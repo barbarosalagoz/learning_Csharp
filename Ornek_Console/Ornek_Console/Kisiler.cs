1a854c9c87db2f8305f7e74988d65d2be5e46f56
Barbaros Emre Alagöz <bealagoz@aof.anadolu.edu.tr>
2025-10-31 16:44:13 +0300

    Add files via upload
    Learning C#

---
+++ Ornek_Console/Ornek_Console/Kisiler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek_Console
+{
+    internal class Kisiler
+    {
+        public void kisilistesi(string adsoyad, string meslek, int yas)
+        {
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i + ".Kişi");
+            //}
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(adsoyad);
+                Console.WriteLine(meslek);
+                Console.Write(yas);
+            }
+
+
+        }
+    }
+}
---
+++ Ornek_Console/Ornek_Console/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Ornek_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Merhaba Dünya!");
+
+            //string kelime;
+            //kelime = "29 Ekim";
+            //Console.WriteLine(kelime);
+
+
+            //int sayi = 5;
+            //Console.Write(sayi);
+
+
+            //Console.Read();
+
+
+            //ks.kisilistesi("Azem Yücedağ");
+            //ks.kisilistesi("Barbaros ALAGÖZ");
+
+            //string ads;
+            //Console.Write("isim girin:");
+            //ads = Console.ReadLine();
+            //ks.kisilistesi(ads);
+
+            //Kisiler ks = new Kisiler();
+            //string adsoyad, meslek;
+            //int yas;
+            
+            //Console.WriteLine("isim girin: ");
+            //adsoyad = Console.ReadLine();
+
+            //Console.WriteLine("meslek girin: ");
+            //meslek = Console.ReadLine();
+
+            //Console.WriteLine("yaş girin: ");
+            //yas = int.Parse(Console.ReadLine());
+
+            //ks.kisilistesi(adsoyad, meslek, yas);
+
+            // üstteki kısım ödevdi
+
+            // ger
+
+            islem isl = new islem();
+            isl.topla(7,6);
+            isl.topla(9, 5);
+            isl.kare(4);
+
+            Console.Read();
+
+        }
+    }
+}
