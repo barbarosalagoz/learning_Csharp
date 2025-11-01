bfb452496b450159f051f59ff339b3ef2f90f7f0
Barbaros Emre Alagöz <bealagoz@aof.anadolu.edu.tr>
2025-11-02 02:18:48 +0300

    Add files via upload
    Learning C#

---
+++ Personel_Kayıt/Personel_Kayıt/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Personel_Kayıt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection baglanti = new SqlConnection("Data Source = Barbaros; Initial Catalog = PersonelVeriTabani; Integrated Security = True; TrustServerCertificate=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            this.tbl_PersonelTableAdapter.Fill(this.personelVeriTabaniDataSet.Tbl_Personel);
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Tbl_Personel (PerAd, PerSoyad, PerSehir, PerMaas, PerMeslek) values (@p1, @p2, @p3, @p4, @p5)", baglanti);
+            komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3", CmbSehir.Text);
+            komut.Parameters.AddWithValue("@p4", MskMaas.Text);
+            komut.Parameters.AddWithValue("@p5", TxtMeslek.Text);
+            komut.ExecuteNonQuery(); // ExecuteNonQuery komutu ile sorguyu çalıştırıyoruz.
+            baglanti.Close();
+            MessageBox.Show("Personel eklendi!");
+        }
+    }
+}
