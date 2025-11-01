1d822ab3e7b6cf381510caa576772999d60a5ff7
Barbaros Emre Alagöz <bealagoz@aof.anadolu.edu.tr>
2025-11-02 02:15:18 +0300

    Add files via upload
    Learning C#

---
+++ Formlar_Arası_Veri_Tasima/Formlar_Arası_Veri_Tasima/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Deployment.Application;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formlar_Arası_Veri_Tasima
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 frm = new Form2();
+            //frm.mesaj = textBox1.Text;
+            //frm.Show();
+            //this.Hide();
+
+
+            Form2 fr = new Form2();
+            fr.m1 = textBox1.Text;
+            fr.m2 = textBox2.Text;
+            fr.m3 = textBox3.Text;
+            fr.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Formlar_Arası_Veri_Tasima/Formlar_Arası_Veri_Tasima/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formlar_Arası_Veri_Tasima
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        //public string mesaj;
+
+        public string m1, m2, m3;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //label1.Text = mesaj;
+            listBox1.Items.Add(m3);
+            listBox1.Items.Add(m2);
+            listBox1.Items.Add(m1);
+        }
+    }
+}
